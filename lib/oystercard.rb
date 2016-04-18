b454e0a6dd0e4f7c3710c9d3bf38cc1ac3c757d6
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-18 19:54:49 +0100

    card cannot be topped up beyond max limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,23 @@ class Oystercard
 
   attr_reader :balance
 
+  MAX_LIMIT = 90.00
+
+
   def initialize
     @balance = 0.00
+
   end
 
   def top_up(money)
-    @balance += money
+    fail "Top up amount pushes you over your maximum oyster card limit." if limit_reached?(money)
+
+      @balance += money
+
+    end
+private
+  def limit_reached?(money)
+    @balance + money >= MAX_LIMIT
   end
 
 
