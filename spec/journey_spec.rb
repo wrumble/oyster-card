62c318c828acf9208c4316100f9ec134e8299821
Wayne Rumble <rumblewayne@gmail.com>
2016-04-20 10:23:54 +0100

    journey stuff mid extraction

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,11 +1,18 @@
 require 'journey'
+require 'oystercard'
 
 describe Journey do
+  
+  let(:entrystation) {double :entrystation}
+  let(:exitstation) {double :exitstation}
+  #let(:oystercard) {double(:oystercard, top_up:}
+    oy = Oystercard.new
+
     describe '#journeys' do
       it "stores entry and exit stations in a hash" do
-        subject.top_up(5)
-        subject.touch_in(entrystation)
-        subject.touch_out(exitstation)
+        oy.top_up(5)
+        oy.touch_in(entrystation)
+        oy.touch_out(exitstation)
         expect(subject.journeys).to eq [journey]
       end
 
@@ -16,26 +23,26 @@ describe Journey do
 
     describe '#journey_history' do
       it 'stores all journeys in journey_history' do
-        subject.top_up(10)
-        subject.touch_in(entrystation)
-        subject.touch_out(exitstation)
-        subject.touch_in(entrystation)
-        subject.touch_out(exitstation)
+        oy.top_up(10)
+        oy.touch_in(entrystation)
+        oy.touch_out(exitstation)
+        oy.touch_in(entrystation)
+        oy.touch_out(exitstation)
         expect(subject.journeys).to eq [journey, journey]
       end
     end
 
     it "returns false on in_journey" do
-      subject.top_up(5)
-      subject.touch_in(entrystation)
-      subject.touch_out(exitstation)
+      oy.top_up(5)
+      oy.touch_in(entrystation)
+      oy.touch_out(exitstation)
       expect(subject.in_journey?).to eq false
     end
 
     it "remembers the last station" do
-      subject.top_up(5)
-      subject.touch_in(entrystation)
-      subject.touch_out(exitstation)
+      oy.top_up(5)
+      oy.touch_in(entrystation)
+      oy.touch_out(exitstation)
       expect(subject.journeys.first[:exit_station]).to eq exitstation
     end
 
@@ -45,14 +52,14 @@ describe Journey do
       end
 
       it "returns true on in_journey" do
-        subject.top_up(5)
-        subject.touch_in(entrystation)
+        oy.top_up(5)
+        oy.touch_in(entrystation)
         expect(subject.in_journey?).to eq true
       end
 
       it "remembers the station after it touched in" do
-        subject.top_up(5)
-        subject.touch_in(entrystation)
+        oy.top_up(5)
+        oy.touch_in(entrystation)
         expect(subject.journeys.first[:entry_station]).to eq entrystation
       end
     end
